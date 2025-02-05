7a3cad219963094a3080690de6c27273e8a82343
jakemeinershagen <jakemeinershagen@gmail.com>
2025-02-05 05:34:46 -0600

    world color

--- scripts/room_changer.gd
+++ scripts/room_changer.gd
@@ -3,7 +3,6 @@ extends Node2D
 @export var room_1_cam: Camera2D
 @export var room_2_cam: Camera2D
 
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Player:
 		room_1_cam.enabled = !room_1_cam.enabled
